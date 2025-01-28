8e54d1443b6b1a972234a9b9bcfb38eb92fe3d04
bngams <n.boris@devidia.net>
2025-01-28 14:49:57 +0000

    basic routing

--- apps/fo/src/app/app-routing.module.ts
+++ apps/fo/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-const routes: Routes = [];
+// type Routes = Route[]
+const routes: Routes = [
+  { path:'', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: NotFoundComponent }, // '**' like Regex captures all
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

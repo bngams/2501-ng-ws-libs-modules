1ff7c4a15d41f2ba3e7c3583a56aad4f63c0ec9c
bngams <n.boris@devidia.net>
2025-01-28 12:37:52 +0000

    base config

---
+++ apps/fo/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoreComponent } from 'core'; // ts path config
+
+@NgModule({
+  // define components
+  declarations: [
+    AppComponent
+  ],
+  // load features
+  imports: [
+    BrowserModule, // CommonModule + ApplicationModule + other features (injector)
+    AppRoutingModule,
+    CoreComponent // a standalone component is a module
+],
+  providers: [], // load injectables
+  exports: [],
+  bootstrap: [AppComponent] // start app with
+})
+export class AppModule { }
